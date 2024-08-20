e9bf369900698b38ef7ebec8af9cdad3aed27b99
nishchaykarle <nishchaykarle@gmail.com>
2024-08-20 00:40:19 -0500

    handle form errors new issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,6 @@
-import prisma from '@/prisma/client';
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-
-const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
-});
+import { createIssueSchema } from "@/app/validationSchemas";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,44 +1,67 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
-    return (
-        <form
-            className="max-w-xl space-y-5"
-            onSubmit={handleSubmit(async (data) => {
-                await fetch("/api/issues", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(data),
-                });
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema),
+    });
 
-                router.push("/issues");
-            })}
-        >
-            <TextField.Root placeholder="Title" {...register("title")} />
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => (
-                    <SimpleMDE placeholder="Description" {...field} />
-                )}
-            />
-            <Button> Submit New Issue</Button>
-        </form>
+    const [error, setError] = useState<string>("");
+
+    return (
+        <div className="max-w-xl">
+            {error && (
+                <Callout.Root color="red">
+                    <Callout.Icon>
+                        <InfoCircledIcon />
+                    </Callout.Icon>
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
+            <form
+                className=" space-y-3"
+                onSubmit={handleSubmit(async (data) => {
+                    try {
+                        await axios.post("/api/issues", data);
+                        router.push("/issues");
+                    } catch (error) {
+                        setError("An unexpected error occurred");
+                    }
+                })}
+            >
+                <TextField.Root placeholder="Title" {...register("title")} />
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => (
+                        <SimpleMDE placeholder="Description" {...field} />
+                    )}
+                />
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                <Button> Submit New Issue</Button>
+            </form>
+        </div>
     );
 };
 
